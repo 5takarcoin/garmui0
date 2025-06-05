ad2dfbe31286e1e13fabcda7d0de1881e54b88ad
5takarcoin <5takarcoin@gmail.com>
2025-06-05 22:45:08 +0600

    About us page

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const IMAGE_URL = "https://ik.imagekit.io/5takarcoin/garmui/"
 
+// const getImages  = ["about_left", ]
+
 
 const hero = document.querySelector("main")
+const crazy = document.querySelector(".crazy")
 
 hero.style.backgroundImage = `url(${IMAGE_URL}hero.jpg)`
+crazy.style.backgroundImage = `url(${IMAGE_URL}about-left.png)`
+
+
+const about_vid = document.querySelector(".about-vid")
+about_vid.src = `${IMAGE_URL}about-vid.jpg`
+// about_vid.style.objectFit = "contain"
