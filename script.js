be6206d254e8b6122aa6ee061d0fc7d5c8b4d101
5takarcoin <5takarcoin@gmail.com>
2025-06-06 03:42:18 +0600

    Services page

--- script.js
+++ script.js
@@ -13,3 +13,50 @@ crazy.style.backgroundImage = `url(${IMAGE_URL}about-left.png)`
 const about_vid = document.querySelector(".about-vid")
 about_vid.src = `${IMAGE_URL}about-vid.jpg`
 // about_vid.style.objectFit = "contain"
+
+const services = [
+  {
+    imgSrc: "knitting.jpg",
+    title: "Knitting",
+    description: "Soft and Sustainable Fabrics"
+  },
+  {
+    imgSrc: "cutting.jpg",
+    title: "Cutting",
+    description: "Precision That Shapes Perfection"
+  },
+  {
+    imgSrc: "dyeing.jpg",
+    title: "Dyeing",
+    description: "Eco-Friendly, Vibrant Colors"
+  },
+  {
+    imgSrc: "sewing.jpg",
+    title: "Sewing",
+    description: "Skilled Hands, Seamless Results"
+  },
+  {
+    imgSrc: "printing.jpg",
+    title: "Printing",
+    description: "Creative, Durable Prints"
+  },
+  {
+    imgSrc: "embroidery.jpg",
+    title: "Embroidery",
+    description: "Elegant Detailing, Every Stitch"
+  }
+];
+
+const container = document.querySelector(".service-cards-container");
+
+container.innerHTML = services.map(service => `
+  <div class="service-card">
+    <img class="service-card-img" src="${IMAGE_URL}services/${service.imgSrc}" alt="${service.title}">
+    <h3>${service.title}</h3>
+    <p>${service.description}</p>
+    <button class="service-card-button">
+      <span>Read More</span>
+      <span><img src="public/arrow_circle_right.svg" alt="Arrow Icon"></span>
+    </button>
+  </div>
+`).join('');
